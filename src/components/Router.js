a8140a4d00979ddd7f3722f18f5428e3b8c958b3
jjokerboxx <dlrlgns58@gmail.com>
2021-10-05 17:02:08 +0900

    adding auth service but need to put more persistence things later

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "routes/Home";
 import Auth from "../routes/Auth";
 
  const AppRouter = ({ isLoggedin }) => {
     return(
         <Router>
             <Switch>
+                {/* 로그인 정보를 받아서 null이 아니면 Auth 화면으로 처리해줌 */}
                 {isLoggedin ? (
                     <>
-                    <Route exact path="/"></Route>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
                     </>
                 ) : (
                     <Route exact path="/">
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "fbase";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccout] = useState(true);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let authData;
+            if(newAccount){
+                //auth.createUserWithEmailAndPassword 
+                authData = await createUserWithEmailAndPassword(authService, email, password);
+            } else {
+                authData = await signInWithEmailAndPassword(authService, email, password);
+            }
+            console.log(authData);
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+
+    const onChange = (e) => {
+        //destructring event
+        const {target : {name, value},} = e;
+        if (name === "email"){
+            setEmail(value);
+        }
+        else if (name === "password") {
+            setPassword(value);
+        }
+    }
+
+    return (<div>
+    <span>Auth</span>
+    <div>
+        <form onSubmit={onSubmit}>
+            <input type="email" id="id" name="email" placeholder="Email" required value={email} onChange={onChange}></input>
+            <input type="text" id="pw" name="password" placeholder="Password" required value={password} onChange={onChange}></input>
+            <input type="submit" value={newAccount ? "Create Account" : "Login"}></input>
+        </form>
+        <div>
+            <button>Continue with Goolge</button>
+            <button>Continue with Github</button>
+        </div>
+    </div>
+
+</div>)};
 export default Auth;
