1ac2bc8bb3f252bc11bcbb3cc7364a4864154841
jjokerboxx <dlrlgns58@gmail.com>
2021-11-01 17:35:27 +0900

    add css file to module the styles and apply Toss font

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,6 +9,7 @@ import {
 } from "@firebase/firestore";
 import React, { useState, useEffect, useRef } from "react";
 import PostIt from "./PostIt";
+import "../styles/App.css";
 
 const Home = ({ userObj }) => {
 	// 0) 상태 모음집
@@ -16,23 +17,26 @@ const Home = ({ userObj }) => {
 	const [postArry, setPostArry] = useState([]);
 	const [isSorted, setIsSorted] = useState(false);
 
-	// 2) 트윗 업로드 버튼 함수
+	// 2)  업로드 버튼 함수
 	const onSubmit = async (e) => {
 		setIsSorted((prev) => prev);
 		e.preventDefault();
-		const $post = document.querySelector(".post-it");
+		const $post = document.querySelector(".postIt");
+		const {
+			style: { backgroundColor },
+		} = $post;
 		const postObj = {
-			// 3) 상태를 객체로 받아와서 파이어스토어에 올리기
+			// 3) 상태를 객체로 받아와서 firestore에 올리기
 			contents: post,
 			uploadedAt: Date.now(),
 			author: userObj.uid,
-			color: $post.style.backgroundColor,
+			color: backgroundColor,
 			like: 0,
 		};
 		const docRef = await addDoc(collection(dbService, "Post"), postObj);
 		console.log("Document written with ID: ", docRef.id);
 		// 4) 입력칸 비우기
-		document.querySelector("#post").value = "";
+		document.querySelector(".post").value = "";
 		setPost("");
 	};
 
@@ -49,7 +53,7 @@ const Home = ({ userObj }) => {
 	};
 
 	useEffect(() => {
-		// 실시간 트윗 렌더링
+		// 실시간 snapshot 렌더링
 		const sortOrder = isSorted ? "like" : "uploadedAt";
 		console.log(sortOrder);
 		const q = query(collection(dbService, "Post"), orderBy(sortOrder, "desc"));
@@ -65,57 +69,22 @@ const Home = ({ userObj }) => {
 	}, [isSorted]);
 	return (
 		<>
-			<button onClick={onSortClick}>Sort</button>
-			<div
-				className='flex-container'
-				style={{ display: "flex", flex: 1, flexDirection: "row" }}>
+			<button className='defaultButton' onClick={onSortClick}>
+				Sort
+			</button>
+			<div className='flexContainer'>
 				<div style={{ flex: 1, margin: 30 }}>
-					<form
-						style={{
-							display: "flex",
-							flexDirection: "column",
-							justifyContent: "center",
-							alignItems: "center",
-						}}>
-						<div
-							className='post-it'
-							style={{
-								display: "flex",
-								flexDirection: "column",
-								backgroundColor: "#ffd359",
-								borderRadius: (20, 20, 10, 10),
-								width: 270,
-								height: 170,
-							}}>
+					<form className='postForm'>
+						<div className='postIt' style={{ backgroundColor: "#ffd359" }}>
 							<textarea
-								style={{
-									backgroundColor: "#ffd359",
-									border: "none",
-									borderRadius: (20, 20, 10, 10),
-									padding: 10,
-									outlineStyle: "none",
-									resize: "none",
-								}}
+								className='post'
 								cols={30}
 								rows={7}
-								id='post'
 								value={post}
 								onChange={onChange}
 								placeholder="What's on your mind?"
 								maxLength={100}></textarea>
-							<div
-								className='color-palette'
-								style={{
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-									backgroundColor: "#fffafa",
-									overflow: "visible",
-									borderRadius: 20,
-									marginLeft: 20,
-									marginRight: 20,
-									boxShadow: "1px 3px 1px #f0f0f0",
-								}}>
+							<div className='colorPalette'>
 								<ColorButton color='#ffd359' />
 								<ColorButton color='#e2ff3d' />
 								<ColorButton color='#ff8547' />
@@ -123,23 +92,12 @@ const Home = ({ userObj }) => {
 								<ColorButton color='#ff8adc' />
 							</div>
 						</div>
-						<button
-							style={{
-								width: 100,
-								border: "none",
-								color: "white",
-								backgroundColor: "#3f7bf2",
-								padding: 5,
-								borderRadius: 10,
-								margin: 10,
-							}}
-							onClick={onSubmit}>
+						<button className='defaultButton' onClick={onSubmit}>
 							Post!
 						</button>
 					</form>
 				</div>
-				<div className='post-div' style={{ flex: 3 }}>
-					{/* 상태를 JSX 반복문으로 렌더 */}
+				<div className='postDiv' style={{ flex: 3 }}>
 					{postArry.map((element) => (
 						<PostIt
 							key={element.id}
@@ -159,8 +117,8 @@ const ColorButton = ({ color }) => {
 		const {
 			target: { id },
 		} = e;
-		const $clickedPost = document.querySelector("#post");
-		const $clickedPostIt = document.querySelector(".post-it");
+		const $clickedPost = document.querySelector(".post");
+		const $clickedPostIt = document.querySelector(".postIt");
 
 		$clickedPost.animate(
 			{ backgroundColor: id },
@@ -178,15 +136,8 @@ const ColorButton = ({ color }) => {
 	return (
 		<div
 			id={color}
-			style={{
-				display: "block",
-				backgroundColor: color,
-				borderRadius: 50,
-				margin: 5,
-				height: 15,
-				width: 15,
-				boxShadow: "1px 3px 1px #f0f0f0",
-			}}
+			className='colorButton'
+			style={{ backgroundColor: color }}
 			onClick={onColorClick}></div>
 	);
 };
